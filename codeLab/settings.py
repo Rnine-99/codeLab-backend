12f8958db34c12724acd0385c9c5de36844269b8
yyz <2842418411@qq.com>
2022-10-30 17:13:48 +0800

    进行去敏

--- codeLab/settings.py
+++ codeLab/settings.py
@@ -9,18 +9,21 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import yaml
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+with open("./secrets.yaml", 'r') as f:
+    SECRETS = yaml.safe_load(f)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y%=c$v^px7_v5#xw-j8twf*j46)c^%#n!odzimcn1k35%nf1+7'
+SECRET_KEY = SECRETS['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +78,7 @@ WSGI_APPLICATION = 'codeLab.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': SECRETS['database']
 }
 
 
