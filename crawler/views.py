d713ee5b62c6751906e09b6747c4d67fa6fd2fc4
Deeeeex <482788525@qq.com>
2022-11-15 17:54:05 +0800

    添加views接口:get_info_repo_name(request, owner, repo_name)

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import json
+from demoSpider import crawl_owner_repo_name, crawl_language
+import yaml
 
-# Create your views here.
+
+def get_info_repo_name(request, owner, repo_name):
+    # Modify the GitHub token value
+    with open('../secrets.yaml', 'r') as f:
+        SECRETS = yaml.safe_load(f)
+    secret_key = SECRETS['SECRET_KEY']
+    headers = {'User-Agent': 'Mozilla/5.0',
+               'Authorization': secret_key,
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+               }
+    owner = 'vuejs'
+    repo_name = 'vue'
+    results = crawl_owner_repo_name(headers, owner, repo_name)
+    print("full_name:", results['full_name'])
+    print("description:", results['description'])
+    print("stargazers_count:", results['stargazers_count'])
+    print("forks_count:", results['forks_count'])
+    print("open_issues_count:", results['open_issues_count'])
+    print("subscribers_count:", results['subscribers_count'])
+    print("git_url:", results['git_url'])
+    print("clone_url:", results['clone_url'])
+    print("ssh_url:", results['ssh_url'])
+
