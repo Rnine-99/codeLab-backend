103df7928a31f2235a329994e3ff45fa1b53bbf4
yyz <2842418411@qq.com>
2022-12-13 13:21:32 +0800

    1

--- contents/utils.py
+++ contents/utils.py
@@ -76,6 +76,42 @@ def crawl_pr_detail(owner, repo_name, pr):
     result = response.decode()
     return result
 
+
+def crawl_pr_comment(owner, repo_name, pr):
+    with open('./secrets.yaml', 'r') as f:
+        SECRETS = yaml.safe_load(f)
+    secret_key = SECRETS['AUTHORIZATION_CODE']
+    headers = {'User-Agent': 'Mozilla/5.0',
+               'Authorization': secret_key,
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+               }
+    url = 'https://api.github.com/repos/{owner}/{repo_name}/pulls/{id}/comments'. \
+        format(owner=owner, repo_name=repo_name, id=pr)
+    # print(url)
+    req = Request(url, headers=headers)
+    response = urlopen(req).read()
+    result = response.decode()
+    return result
+
+
+def crawl_issue_comment(owner, repo_name, pr):
+    with open('./secrets.yaml', 'r') as f:
+        SECRETS = yaml.safe_load(f)
+    secret_key = SECRETS['AUTHORIZATION_CODE']
+    headers = {'User-Agent': 'Mozilla/5.0',
+               'Authorization': secret_key,
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+               }
+    url = 'https://api.github.com/repos/{owner}/{repo_name}/issues/{id}/comments'. \
+        format(owner=owner, repo_name=repo_name, id=pr)
+    # print(url)
+    req = Request(url, headers=headers)
+    response = urlopen(req).read()
+    result = response.decode()
+    return result
+
 # 传入项目模型
 def crawl_new_issue_or_pr(program):
     a = json.loads(crawl_issue_by_id(program.owner, program.repo_name))
@@ -84,16 +120,17 @@ def crawl_new_issue_or_pr(program):
         issue_biggest = RepoIssue.objects.filter(repo_id=program.id).order_by('-issue_id').first().issue_id
     except:
         issue_biggest = 0
-    page = issue_biggest_id
+    page = 52
     while page > 0:
         try:
             issue = json.loads(crawl_issue_detail(program.owner, program.repo_name, page))
         except:
+            print("error at page %d", page)
             page -= 1
             continue
         if len(issue) == 0:
             return
-        if issue['id'] == issue_biggest:
+        if issue['number'] == issue_biggest:
             return
         if "pull_request" not in issue:
             try:
